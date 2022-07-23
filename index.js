d7c3ae554f230e45061c3ded052575104460aeb1
Rodrigo Henrique <rodhenrc@outlook.com>
2022-07-22 23:47:35 -0300

    Mudança no index.js, adicionado nodemon

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const app = require("./app")
+const express = require("express");
 const WebSocket = require("ws");
+const http = require("http");
+const port = 8080;
 
-const wss = new WebSocket.Server(app)
+const app = express();
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
 wss.on("connection", (ws) => {
-    ws.on("message", (data) => {
-        console.log(`data: ${data}`)
-    })
-})
+  ws.on("message", (data) => {
+    console.log(`Recebi a mensagem: "${data}"`);
+    ws.send("Olá, mensagem recebida!");
+  });
+
+  ws.send("Servidor WebSocket iniciado!");
+});
+
+server.listen(port, () => {
+  console.log(`Server iniciado na porta ${port}`);
+});
