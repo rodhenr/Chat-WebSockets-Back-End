8d4273223f65caf8035c6e8cfbc5bb7b411ae728
Rodrigo Henrique <rodhenrc@outlook.com>
2022-07-23 00:12:45 -0300

    Adicionado broadcast(mensagem entre dois navegadores)

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ const wss = new WebSocket.Server({ server });
 wss.on("connection", (ws) => {
   ws.on("message", (data) => {
     console.log(`Recebi a mensagem: "${data}"`);
-    ws.send("Olá, mensagem recebida!");
+    wss.clients.forEach(function (client) { // arrow function n funciona aqui
+      if (client !== ws) {
+        client.send(`Olá, mensagem do outro usuário: ${data}`);
+      }
+    });
   });
 
   ws.send("Servidor WebSocket iniciado!");
